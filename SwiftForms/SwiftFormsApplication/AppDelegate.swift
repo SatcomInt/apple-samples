529bfa6ed503f9299ab94c11e02ec2e471c8007c
jbarros35 <josecarlos.barros@gmail.com>
2019-10-20 13:20:04 +0100

    [Chore] Added Swift Forms sample.

---
+++ SwiftForms/SwiftFormsApplication/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  SwiftForms
+//
+//  Created by Miguel Angel Ortuno on 20/08/14.
+//  Copyright (c) 2014 Miguel Angel Ortuño. All rights reserved.
+//
+import IQKeyboardManager
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : Any]?) -> Bool {
+        IQKeyboardManager.shared().isEnabled = true
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
+
